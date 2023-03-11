08beb925b863270c479856b84636f7d11509a78e
Rahul Pahuja <rahulpahuja2015@gmail.com>
2023-03-11 17:13:18 +0530

    Added Sheet Component

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ function App() {
           
         </a>
       </header>
+      <body>
+        <Sheet/>
+      </body>
     </div>
   );
 }
---
+++ src/components/Sheet.js
@@ -0,0 +1,11 @@
+
+
+class Sheet extends React.Component{
+    render(){
+        return {
+            <div>
+            <h3>Sheet Title</h3>
+            </div>
+        }
+    }
+}

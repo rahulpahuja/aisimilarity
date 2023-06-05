874a1d46c8552ac6c1951b149bae90e990cc4251
Rahul Pahuja <rahulpahuja2015@gmail.com>
2023-06-05 17:32:07 +0530

    Changed the whole project

--- src/components/Sheet.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Sheet extends React.Component{
-    render(){
-        return <h3 >T {this.props.title}</h3>;
-    }
-}
---
+++ src/test/java/com/aisimilarity/AISimilarity/AiSimilarityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisimilarity.AISimilarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSimilarityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

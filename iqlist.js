a26cc8fe7b23ef0a06e6f220a48940688fcb7e17
Rahul Pahuja <rahulpahuja2015@gmail.com>
2025-07-13 20:25:58 +0530

    Update iqlist.js

--- iqlist.js
+++ iqlist.js
@@ -1,3 +1,27 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
   }
+
+// Add an event listener to the toggle switch
+function checkChange() {
+    // Get the toggle switch element and the body
+    const toggleSwitch = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    // Load the user's preference from localStorage
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        body.classList.add('dark-mode');
+        toggleSwitch.checked = true;
+    }
+    // Get the toggle switch element and the body
+
+    if (toggleSwitch.checked) {
+        body.classList.add('dark-mode');
+        localStorage.setItem('dark-mode', 'disabled');
+
+    } else {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('dark-mode', 'enabled');
+    }
+    console.log("Hitting here")
+}
